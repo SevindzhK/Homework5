0ab5d2e48840b4a8117b2e4b6cc856e638053ceb
SevindzhK <sevinge0709@gmail.com>
2022-09-19 19:55:41 +0300

    Added Ex36

---
+++ Ex36/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+int[] CreateArray()
+{
+    Random rnd = new Random(); 
+    int[] array = new int[8]; 
+    
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(-10,10); 
+    }
+    return array;
+}
+
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} \t"); 
+    }
+}
+
+
+int SumUnevenIndex(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 != 0) sum += array[i];             
+    }      
+    return sum; 
+}
+
+int[] arr = CreateArray();
+PrintArray(arr);
+Console.WriteLine();
+Console.WriteLine($"Sum of numbers with uneven index: {SumUnevenIndex(arr)}"); 
