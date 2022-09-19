a9bb382104eb15ccdca1a0efdb40145e0dc9cf21
SevindzhK <sevinge0709@gmail.com>
2022-09-19 20:33:02 +0300

    Added Ex38

---
+++ Ex38/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+double[] CreateArray()
+{
+    Random rnd = new Random(); 
+    double[] array = new double[8]; 
+    
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(10,100)/10.0; 
+    }
+    return array;
+}
+
+
+void PrintArray(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} \t"); 
+    }
+}
+
+
+double DifferenceMaxMin(double[] array)
+{
+    double max = array[0];
+    double min = array[0];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > max) max = array[i];    
+        if (array[i] < min) min = array[i];         
+    }      
+    return max - min; 
+}
+
+double [] arr = CreateArray();
+PrintArray(arr);
+Console.WriteLine();
+Console.WriteLine($"The different between max and min: {DifferenceMaxMin(arr)}"); 
